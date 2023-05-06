6268bec4753002f1e02430a57a1e5fc3827a8dcc
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2023-05-07 01:14:00 +0900

    fix

--- game.sol
+++ game.sol
@@ -1,42 +1,86 @@
-//SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 contract RockPaperScissors {
-    enum Choice { None, Rock, Paper, Scissors }
-    enum Outcome { None, Draw, PlayerWins, ComputerWins }
+    address public owner;
 
-    uint public playerBalance;
-    uint public computerBalance;
-    Choice public playerChoice;
-    Choice public computerChoice;
-    Outcome public outcome;
+    enum Move {NONE, ROCK, PAPER, SCISSORS}
 
-    function play(uint choice) public payable {
-        require(msg.value == 0.1 ether, "Please send exactly 0.1 ether");
-        require(playerBalance == 0 && computerBalance == 0, "Game in progress");
-        require(choice >= uint(Choice.Rock) && choice <= uint(Choice.Scissors), "Invalid choice");
+    struct Game {
+        address player;
+        Move playerMove;
+        Move contractMove;
+        uint256 amount;
+        bool played;
+    }
 
-        playerBalance = msg.value;
-        playerChoice = Choice(choice);
+    mapping(bytes32 => Game) public games;
 
-        computerBalance = msg.value;
-        computerChoice = Choice(uint(keccak256(abi.encodePacked(block.timestamp, block.basefee, address(this)))) % 3);
+    event GameCreated(bytes32 indexed gameId, address indexed player, uint256 amount);
+    event GamePlayed(bytes32 indexed gameId, address indexed player, uint8 playerMove, uint8 contractMove, uint256 amount, bool win);
 
-        uint winner = (uint(playerChoice) - uint(computerChoice) + 3) % 3;
-        if (winner == 0) {
-            outcome = Outcome.Draw;
-            playerBalance = 0;
-            computerBalance = 0;
-        } else if (winner == 1) {
-            outcome = Outcome.PlayerWins;
-            playerBalance = playerBalance * 2;
-            computerBalance = 0;
-            payable(msg.sender).transfer(playerBalance);
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createGame(bytes32 gameId) public payable {
+        require(msg.value > 0, "Amount should be greater than 0");
+        require(games[gameId].player == address(0), "Game already exists");
+        games[gameId].player = msg.sender;
+        games[gameId].amount = msg.value;
+        emit GameCreated(gameId, msg.sender, msg.value);
+    }
+
+    function play(bytes32 gameId, uint8 playerMove) public payable returns (int8) {
+        require(games[gameId].played == false, "Game already played");
+        require(games[gameId].player != address(0), "Game does not exist");
+        require(msg.value == games[gameId].amount, "Amount should be equal to the game amount");
+
+        // Set player move
+        if (playerMove == 1) {
+            games[gameId].playerMove = Move.ROCK;
+        } else if (playerMove == 2) {
+            games[gameId].playerMove = Move.PAPER;
+        } else if (playerMove == 3) {
+            games[gameId].playerMove = Move.SCISSORS;
+        }
+
+        // Generate contract move
+        uint256 seed = uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp, block.number)));
+        uint8 contractMove = uint8(seed % 3) + 1;
+        if (contractMove == 1) {
+            games[gameId].contractMove = Move.ROCK;
+        } else if (contractMove == 2) {
+            games[gameId].contractMove = Move.PAPER;
+        } else if (contractMove == 3) {
+            games[gameId].contractMove = Move.SCISSORS;
+        }
+
+        // Determine winner
+        int8 result = determineWinner(games[gameId].playerMove, games[gameId].contractMove);
+
+        // Update game state
+        games[gameId].played = true;
+
+
+        // Emit event
+        emit GamePlayed(gameId, games[gameId].player, playerMove, contractMove, games[gameId].amount, result == 1);
+
+        return result;
+    }
+
+    function determineWinner(Move playerMove, Move contractMove) private pure returns (int8) {
+        if (playerMove == Move.ROCK && contractMove == Move.SCISSORS) {
+            return 1;
+        } else if (playerMove == Move.PAPER && contractMove == Move.ROCK) {
+            return 1;
+        } else if (playerMove == Move.SCISSORS && contractMove == Move.PAPER) {
+            return 1;
+        } else if (playerMove == contractMove) {
+            return 0;
         } else {
-            outcome = Outcome.ComputerWins;
-            playerBalance = 0;
-            computerBalance = computerBalance * 2;
+            return -1;
         }
     }
 }
+
